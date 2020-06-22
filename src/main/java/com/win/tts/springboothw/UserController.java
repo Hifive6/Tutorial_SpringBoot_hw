79761defb0afcbcfeb99d297ad04483985d32c18
David Juarez <djuar05@gmail.com>
2020-06-22 14:08:51 -0400

    create a get method for info by specific id

--- src/main/java/com/win/tts/springboothw/UserController.java
+++ src/main/java/com/win/tts/springboothw/UserController.java
@@ -1,26 +1,42 @@
 package com.win.tts.springboothw;
 
 import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class UserController {
     
+    private UserRepository userRepository;
 
     @GetMapping("/signup")
     public String showSignUpForm(User user){
         return "add-user";
     }
-
+    
     @PostMapping("/adduser")
     public String addUser(@Valid User user, BindingResult result, Model model){
         if(result.hasErrors()){
             return "add-user";
         }
+        
+        
         userRepository.save(user);
         model.addAttribute("user", userRepository.findAll());
         return "redirect:/index";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model){
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+
+        model.addAttribute("user", user);
+        return "update-user";
     }
 }
